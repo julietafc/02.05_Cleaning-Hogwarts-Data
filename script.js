3cace155544fd51470b8c7273af06bc55b5b306d
julietafc <juli47g1@stud.kea.dk>
2021-09-03 21:57:29 +0200

    match function nor working in nickname

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function prepareObjects(jsonData) {
     student.nickname = getNickName(object.fullname);
     student.lastname = getLastName(object.fullname);
     student.house = getHouse(object.house);
+    student.image = getImage(object);
     allStudents.push(student);
   });
 }
@@ -44,14 +45,19 @@ function getFirstName(fullName) {
 
 function getMiddleName(fullName) {
   const noSpace = fullName.trimStart(fullName);
+
   const middleName = noSpace.substring(noSpace.indexOf(" ") + 1, noSpace.lastIndexOf(" "));
+  // // if statement if the student doesn't have middlename
+  // const quotes = fullName.subtring(fullName.match(/\("[^)]+"\)/)[1]);
   const initial = middleName.substring(0, 1).toUpperCase() + middleName.substring(1).toLowerCase();
   return initial;
 }
 
+// getting nickname
 function getNickName(fullName) {
   //   const nosign = fullName.substring(fullName.split(""""));
   //   return nosign;
+  // return quotes;
 }
 
 function getLastName(fullName) {
@@ -59,6 +65,14 @@ function getLastName(fullName) {
   const lastName = noSpace.substring(noSpace.lastIndexOf(" ") + 1);
   const initial = lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
   return initial;
+}
+
+function getImage() {
+  //   let img = document.createElement('img');
+  //   img.src =
+  // "'${lastName}`.png";
+  //   document.getElementById('body').appendChild(img);
+  //   down.innerHTML = "Image Element Added.";
 }
 
 function getHouse(house) {
